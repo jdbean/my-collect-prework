f0dbbd35e3e85cb4f4cf53259e040b3e65bb623f
J.D. Bean <jonathandbean@gmail.com>
2018-02-16 12:27:21 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(collection)
+  i = 0
+  new_collection = []
+
+  while i < collection.length
+    yield collection[i]
+    new_collection << collection[i]
+    i = i + 1
+  end
+  
+  array
+end
 
 
